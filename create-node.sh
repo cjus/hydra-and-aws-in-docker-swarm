2bf17238d92d6d62dff49a1ecc78904a27eb53e2
Carlos Justiniano <carlos.justiniano@gmail.com>
2017-03-18 00:01:52 -0400

    Create create-node.sh

---
+++ create-node.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AWS_AK="FAKE-AKIAISQOOR3SGN3RPCBT"
+AWS_SK="FAKE-CfZ77GEyomrp7+VmRCX+yq+D6uJoKv8lufbEH8Jl"
+AWS_VPC='vpc-{here}'
+AWS_REGION='us-east-1'
+
+NODE_NAME=$1
+MTYPE=$2
+MACHINE_TYPE=${MTYPE:='t2.small'}
+
+docker-machine create --driver amazonec2 \
+	--amazonec2-access-key $AWS_AK \
+	--amazonec2-secret-key $AWS_SK \
+	--amazonec2-vpc-id "${AWS_VPC}" \
+	--amazonec2-region "${AWS_REGION}" \
+	--amazonec2-instance-type "${MACHINE_TYPE}" \
+	${NODE_NAME}
+
+echo "${NODE_NAME} should be available in a minute"
